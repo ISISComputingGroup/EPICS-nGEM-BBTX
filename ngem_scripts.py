f6f5e3e979a31e983ac81d07a067a55c669c4103
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2022-07-05 19:07:33 +0100

    Add fileprefix

--- ngem_scripts.py
+++ ngem_scripts.py
@@ -10,3 +10,10 @@ def start_ngem():
 def stop_ngem():
     g.set_pv("NGEM_01:STOP:SP",1, is_local=True)
 
+# write a tiff file of current image data
+def write_ngem_file():
+    g.set_pv("NGEM_01:WRITEFILE:SP",1, is_local=True)
+
+# change prefix for tiff file of current image data
+def set_ngem_file_prefix():
+    g.set_pv("NGEM_01:FILEPREFIX:SP",1, is_local=True)
