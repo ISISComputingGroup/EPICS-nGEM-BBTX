23f3dc78e0addde3568d219ba6f7f26a23fcef66
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-03-30 11:27:16 +0100

    Correct if clause syntax

--- util/copycmd.bat
+++ util/copycmd.bat
@@ -1,5 +1,6 @@
 REM @echo off
 REM reset PATH to default as EPICS path is long and can cause issues
+setlocal
 set "PATH=C:\windows\system32;C:\windows;C:\windows\System32\Wbem"
 set "BASE=%1"
 set "DIR=%2"
@@ -11,7 +12,7 @@ set "ARCHIVE=%5"
 set "INSTDIR=%INSTNAME%%INSTRUN%"
 REM example archive directory, change locally
 
-if "%ARCHIVE%" nes "" (
+if not "%ARCHIVE%" == "" (
     REM wait 30 seconds
     timeout /t 30 /nobreak >NUL
     robocopy "%BASE%\%DIR%" "%ARCHIVE%\%INSTDIR%\%DIR%" *.* /MOV /E
